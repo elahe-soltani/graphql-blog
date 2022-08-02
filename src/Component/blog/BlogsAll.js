60323e6068f7ab526c5f8edfb52498a8239f4e8a
Elahe Soltani <elahesoltani196@gmail.com>
2022-08-03 01:37:51 +0430

    blogs page is add

--- src/Component/blog/BlogsAll.js
+++ src/Component/blog/BlogsAll.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import { useQuery } from '@apollo/client';
+import { Grid } from '@mui/material';
+import { GET_BLOGS_INFO } from '../../graphql/queries';
+import CardEL from '../shared/CardEL';
+import Loader from '../shared/Loader';
 const BlogsAll = () => {
-    return (
-        <div>
-            
-        </div>
+    const {loading, data ,errors}=useQuery(GET_BLOGS_INFO);
+    if (loading) return  <Loader />
+    if (errors) return <h3>Errors...</h3>
+    return(
+       <Grid container spacing={5} p={8}>
+        {data.posts.map((post) => (
+          <Grid item xs={12} sm={6} md={4} key={post.id}>
+               <CardEL {...post} />
+            </Grid> ) )}
+       </Grid>
     );
 };
 

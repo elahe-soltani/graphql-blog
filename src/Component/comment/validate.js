5744ae047799ef911b72e9e88b83da9de23655f3
Elahe Soltani <elahesoltani196@gmail.com>
2022-08-03 01:46:13 +0430

    validate form

---
+++ src/Component/comment/validate.js
@@ -0,0 +1,25 @@
+export const validate = data => {
+    
+    const error={};
+
+    if (!data.name.trim()){
+        error.name="نام را وارد کنید";
+    } else {
+        delete error.name;
+    }
+
+    if (!data.email){
+        error.email = "ایمیل را وارد کنید";
+    } else if (! /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ .test(data.email)){
+        error.email="ادرس ایمیل نامعتبر است"
+    } else {
+        delete error.email;
+    }
+
+    if (!data.text.trim()){
+        error.text="کامنت را وارد کنید";
+    } else {
+        delete error.text;
+    }
+    return error;
+}
